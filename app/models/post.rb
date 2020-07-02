bfc9cdb4824251e8181757f1ea573ccf02648fd5
paul_vonhagen <pvonhagen26@gmail.com>
2020-07-01 19:06:44 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |cat_att|
+      if cat_att["name"].present?
+        category = Category.find_or_create_by(cat_att)
+        self.categories << category
+      end
+    end
+  end
 
 end
